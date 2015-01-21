f657c7a731efe2f5d34088e2f03aa41767dbd30b
mindblank <dnickz25@gmail.com>
2015-01-21 10:25:05 +0800

    Jan 20 Clear Files

--- wp-content/themes/guitarinstructors-archive/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-<div>
-<input class="text" type="text" value="<?php if(trim(wp_specialchars($s,1))!='') echo trim(wp_specialchars($s,1));else echo 'Search...';?>" name="s" id="s" placeholder="Search..." />
-<input type="submit" class="submit" name="Submit" value="" />
-</div>
-</form>
